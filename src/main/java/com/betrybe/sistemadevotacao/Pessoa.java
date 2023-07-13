340956b69469be01cdd301887b89123032574eda
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-07-12 22:04:33 -0300

    Requisitos 1, 2 e 3 Finalizados.

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,18 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+  private String nome;
+
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return nome;
+  }
 }
+
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
 
 }
