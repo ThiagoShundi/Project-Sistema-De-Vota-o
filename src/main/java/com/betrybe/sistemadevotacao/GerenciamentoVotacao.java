66ea46ca040e38059863e5ddb1819ed7ff169eb2
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-07-13 00:53:03 -0300

    Requisito 4 e 5 Finalizados.

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,50 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
+/**
+ * Class GerenciamentoVotacao.
+ */
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  /**
+   * Método cadastrarPessoaCandidata.
+   */
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+      }
+    }
+    {
+      PessoaCandidata novaPessoaCandidata = new PessoaCandidata(nome, numero);
+      pessoasCandidatas.add(novaPessoaCandidata);
+    }
+  }
+
+  /**
+   * Método cadastrarPessoaEleitora.
+   */
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora eleitora : pessoasEleitoras) {
+      if (eleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+      }
+    }
+    {
+      PessoaEleitora novaPessoaEleitora = new PessoaEleitora(nome, cpf);
+      pessoasEleitoras.add(novaPessoaEleitora);
+    }
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  public void mostrarResultado() {
+
+  }
 }
